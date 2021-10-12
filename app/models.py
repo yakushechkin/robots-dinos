bf0945432912104a7576d4f5ae49d464b8a931e2
yakushechkin <yakushechkin.dmitry@gmail.com>
2021-10-12 16:52:09 +0200

    add a simulation space

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 import enum
-# from datetime import datetime 
+
+# from datetime import datetime
+
 
 class Direction(enum.Enum):
     east = 0
@@ -9,28 +11,41 @@ class Direction(enum.Enum):
     south = 2
     north = 3
 
+
 db = SQLAlchemy()
 
+
 class Robot(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
     x = db.Column(db.Integer, nullable=False)
     y = db.Column(db.Integer, nullable=False)
     direction = db.Column(db.Enum(Direction), nullable=False)
-    # created = db.Column(db.DateTime, default=datetime.now())
 
-    # def __init__(self, *args, **kwards):
-    #     super().__init__(*args, **kwards)
-        
     def __repr(self):
         return f"Robot(id = {id}, x = {x}, y = {y}, direction = {direction})"
 
+
 class Dino(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
     x = db.Column(db.Integer, nullable=False)
     y = db.Column(db.Integer, nullable=False)
-    # created = db.Column(db.DateTime, default=datetime.now())
-       
+
     def __repr(self):
-        return f"Dino(id = {id}, x = {x}, y = {y})"
+        return f"Dinosaur(id = {id}, x = {x}, y = {y})"
+
+
+class Game(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    dim = db.Column(db.Integer, default=50)
+    n_robots = db.Column(db.Integer, default=0)
+    n_dinos = db.Column(db.Integer, default=0)
+    n_destroyed_dinos = db.Column(db.Integer, default=0)
+
+    def __repr(self):
+        return f"Game(dim = {dim}, n_robots = {n_robots}, n_dinos = {n_dinos}, n_destroyed_dinos = {n_destroyed_dinos})"
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
