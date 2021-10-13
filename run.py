668b7cd9b730c0e3f46e3eb793fe18f3c1d1a8ea
yakushechkin <yakushechkin.dmitry@gmail.com>
2021-10-13 09:58:52 +0200

    add db refreshing option

--- run.py
+++ run.py
@@ -1,20 +1,20 @@
-# run.py
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy  # ORM
 from flask_restful import Api
 
 from config import Config
 
 # import resources
-from app.game import Game_resource
-from app.robots import Robot_resource
-from app.dinos import Dino_resource
+from app.resources.game import GameResource
+from app.resources.robots import RobotResource
+from app.resources.dinos import DinoResource
+
+# uncomment if you want DB to be refreshed each run (1/2)
+# from app.models.models import Dino, Robot, Game
 
 
 app = Flask(__name__)
 api = Api(app)
 app.config.from_object(Config)
-# db = SQLAlchemy(app)
 
 
 @app.before_first_request
@@ -22,13 +22,24 @@ def create_tables():
     db.create_all()
 
 
-api.add_resource(Robot_resource, "/robot/")
-api.add_resource(Dino_resource, "/dino/")
-api.add_resource(Game_resource, "/game/")
+# uncomment if you want DB to be refreshed each run (2/2)
+# clear tables
+# try:
+#     db.session.query(Dino).delete()
+#     db.session.query(Robot).delete()
+#     db.session.query(Game).delete()
+#     db.session.commit()
+# except:
+#     db.session.rollback()
+
+
+api.add_resource(RobotResource, "/robot/")
+api.add_resource(DinoResource, "/dino/")
+api.add_resource(GameResource, "/game/")
 
 
 if __name__ == "__main__":
-    from app.models import db
+    from app.models.models import db
 
     db.init_app(app)
     app.run(port=5000, debug=True)
