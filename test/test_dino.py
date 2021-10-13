9e0f03df215399a39a72e22b4908187c2815505e
yakushechkin <yakushechkin.dmitry@gmail.com>
2021-10-13 10:02:31 +0200

    update tests

--- test/test_dino.py
+++ test/test_dino.py
@@ -1,13 +1,14 @@
-# from main import create_space
-
-# def test_create_space():
-#     assert create_space().shape == (50, 50)
-
 import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"x": 10, "y": 5}, {"x": 2, "y": 5}, {"x": 1, "y": 7}]
+data = [
+    {"x": 10, "y": 5},
+    {"x": 2, "y": 5},
+    {"x": 1, "y": 7},
+    {"x": 1, "y": 1},
+    {"x": 2, "y": 1},
+]
 
 
 for i in range(len(data)):
@@ -15,5 +16,5 @@ for i in range(len(data)):
     response = requests.post(BASE + "dino/", data[i])
     print(response.json())
 
-response = requests.get(BASE + "dino/", {"id": 6})
+response = requests.get(BASE + "dino/", {"id": 4})
 print(response.json())
--- test/test_robots.py
+++ test/test_robots.py
@@ -12,21 +12,28 @@ data = [
     {"x": 2, "y": 6, "direction": "west"},
     {"x": 7, "y": 2, "direction": "east"},
     {"x": 14, "y": 223, "direction": "east"},
+    {"x": 1, "y": 2, "direction": "east"},
 ]
 
-
 # for i in range(len(data)):
-#     print(BASE + "robot/{}".format(str(i)), data[i])
-#     response = requests.put(BASE + "robot/{}".format(str(i)), data[i])
+#     print(BASE + "robot/", data[i])
+#     response = requests.post(BASE + "robot/", data[i])
 #     print(response.json())
-
-for i in range(len(data)):
-    print(BASE + "robot/", data[i])
-    response = requests.post(BASE + "robot/", data[i])
-    print(response.json())
 
 response = requests.get(BASE + "robot/", {"id": 2})
 print(response.json())
+
+data_put = [
+    {"id": 2, "action": "move backward"},
+    {"id": 3, "action": "attack"},
+    {"id": 9, "action": "move forward"},
+    {"id": 1, "action": "delete"},
+]
+
+# for i in range(len(data_put)):
+#     print(BASE + "robot/", data_put[i])
+#     response = requests.put(BASE + "robot/", data_put[i])
+#     print(response.json())
 
 # response = requests.patch(BASE + "robot/2", {"name": "Old name", "likes": 1234, "views": 13})
 # print(response.json())
